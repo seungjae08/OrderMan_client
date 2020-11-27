0803021b62de40e9c932ee2cc6b79cca2396ef94
ggomajae <ggomajae@gmail.com>
2020-11-27 11:34:09 +0900

    [client] fix Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
 type propsType = {
-  children: string
+  children: string,
+  color?:string,
+  bgColor?:string
 }
 export default function Button(props : propsType) {
+
+  let color = props.color? props.color : "white";
+  let bgColor = props.bgColor? props.bgColor : "#ff7f00"
   return (
-    <div className="Button-wrap">
+    <div className="Button-wrap" style={{color, backgroundColor:bgColor}}>
       {props.children}
     </div>
   )
