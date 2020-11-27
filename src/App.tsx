04015572722d5bc69eac2e78280d80e7910e4f79
ggomajae <ggomajae@gmail.com>
2020-11-27 11:45:21 +0900

    [client] apply reset.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Main from 'pages/Main';
 import Login from 'pages/Login'
+import SignUp from 'pages/SignUp';
+import SignUpSocial from 'pages/SignUpSocial';
+import Order from 'pages/Order';
+import OrderOption from 'pages/OrderOption';
+import 'styles/reset.css';
 import 'styles/layout.css';
 import 'styles/common.css';
 import 'styles/App.css';
+
+
 
 
 function App() {
@@ -12,6 +19,11 @@ function App() {
     <div className="App">
       <Route path="/" exact component={Main}/>
       <Route path="/login" component={Login}/>
+      <Route path="/signup/social" component={SignUpSocial}/>
+      <Route path="/signup" component={SignUp}/>
+      <Route path="/order/option" component={OrderOption}/>
+      <Route path="/order" component={Order}/>
+      
     </div>
   );
 }
