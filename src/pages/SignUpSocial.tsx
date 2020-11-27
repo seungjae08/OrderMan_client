a7ca308c2d9f9022f480f3d7be036a715db49351
ggomajae <ggomajae@gmail.com>
2020-11-27 13:38:54 +0900

    [client] fix SignUp, SignUpSocial

--- src/pages/SignUpSocial.tsx
+++ src/pages/SignUpSocial.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Button from 'components/Button'
 
 export default function SignUpSocial() {
   return (
-    <div>
-      
+    <div id="wrap">
+      <div className="mb-view verCenter Login">
+        <div className="inputWrap">
+          <input type="text" placeholder="주소"/>
+          <input type="text" placeholder="상호명"/>
+        </div>
+        <Button>가입하기</Button>
+      </div>
     </div>
   )
 }
