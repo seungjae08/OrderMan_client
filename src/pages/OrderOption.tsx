6079ddcd7f947e5c7130f8b02c0a66353776a075
ggomajae <ggomajae@gmail.com>
2020-11-27 11:34:29 +0900

    [client] set page Component

---
+++ src/pages/OrderOption.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function OrderOption() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/pages/SignUpSocial.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignUpSocial() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
