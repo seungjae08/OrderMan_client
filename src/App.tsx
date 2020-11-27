8d1a6711a726c74f62be71ff060dc36bb152c610
ggomajae <ggomajae@gmail.com>
2020-11-27 11:05:37 +0900

    [client] add Login page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Main from 'pages/Main';
+import Login from 'pages/Login'
 import 'styles/layout.css';
+import 'styles/common.css';
 import 'styles/App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={Main}/>
+      <Route path="/" exact component={Main}/>
+      <Route path="/login" component={Login}/>
     </div>
   );
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from 'components/Button'
+
+export default function Login() {
+  return (
+    <div id="wrap">
+      <div className="mb-view">
+        
+        <Button>로그인</Button>
+      </div>
+    </div>
+  )
+}
+
