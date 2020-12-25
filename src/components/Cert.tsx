c8c5d989a17b9af8a111122b9a854ab9b27b39b5
seungjae <seungjae08>
2020-12-25 12:51:37 +0900

    m

--- src/components/Cert.tsx
+++ src/components/Cert.tsx
@@ -23,33 +23,33 @@ export default function Cert(props: propTypes) {
     return regExp.test(asValue); // 형식에 맞는 경우 true 리턴
   }
 
-  const handleSubmitMobile = useCallback(()=>{
-    if(isCelluar(props.mobile)===false){
-      setCertErrorMsg('양식에 맞게 입력해주세요');
-      return;
-    }
-    //전송
-    fetch(serverPath + '/user/mobile', {
-      method: 'POST',
-      mode: 'cors', 
-      credentials: 'include',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        mobile: props.mobile  
-      })
-    }).then((res)=>{
-      if(res.status===200){
-        //인증번호 정상발송
-        setIsRenderCertInput(true);
-      }else{
-        //발송 미처리
-        setCertErrorMsg('인증번호 발송이 완료되지 않았습니다. 다시 시도해주세요.')
-      }
-    })
-    .catch((e:Error)=>{
-      setCertErrorMsg('인증번호 발송이 완료되지 않았습니다. 다시 시도해주세요.')
-    })
-  },[props.mobile]);
+  // const handleSubmitMobile = useCallback(()=>{
+  //   if(isCelluar(props.mobile)===false){
+  //     setCertErrorMsg('양식에 맞게 입력해주세요');
+  //     return;
+  //   }
+  //   //전송
+  //   fetch(serverPath + '/user/mobile', {
+  //     method: 'POST',
+  //     mode: 'cors', 
+  //     credentials: 'include',
+  //     headers: {'Content-Type': 'application/json'},
+  //     body: JSON.stringify({
+  //       mobile: props.mobile  
+  //     })
+  //   }).then((res)=>{
+  //     if(res.status===200){
+  //       //인증번호 정상발송
+  //       setIsRenderCertInput(true);
+  //     }else{
+  //       //발송 미처리
+  //       setCertErrorMsg('인증번호 발송이 완료되지 않았습니다. 다시 시도해주세요.')
+  //     }
+  //   })
+  //   .catch((e:Error)=>{
+  //     setCertErrorMsg('인증번호 발송이 완료되지 않았습니다. 다시 시도해주세요.')
+  //   })
+  // },[props.mobile]);
 
   const handleSubmitVerifyNumber = useCallback(() => {
     fetch(serverPath + '/user/mobile', {
